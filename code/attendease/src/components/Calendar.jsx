d2236220e11315ae59a4bbc3ff9d3c0af7edac83
Freedteck <freedteck@gmail.com>
2024-09-17 17:44:56 +0100

    Remove attendease

--- code/attendease/src/components/Calendar.jsx
+++
@@ -1,9 +0,0 @@
-import Calendar from "react-calendar";
-import "../styles/calendar.css";
-import "react-calendar/dist/Calendar.css";
-
-const MyCalendar = () => {
-  return <Calendar />;
-};
-
-export default MyCalendar;
